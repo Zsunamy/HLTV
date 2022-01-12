8722a1734ad2a31f41e502e7146d8633ab86442c
Drachenfalke12 <marco.pa@gmx.de>
2022-01-12 18:14:33 +0100

    added allMatchesListed property for events

--- src/endpoints/getEvent.ts
+++ src/endpoints/getEvent.ts
@@ -49,6 +49,7 @@ export interface FullEvent {
   prizePool: string
   location: Country
   numberOfTeams?: number
+  allMatchesListed: boolean
   teams: FullEventTeam[]
   prizeDistribution: FullEventPrizeDistribution[]
   relatedEvents: Event[]
@@ -115,7 +116,9 @@ export const getEvent =
         }
       })
 
-    const numberOfTeams = $('td.teamsNumber').numFromText()!
+    const numberOfTeams = parseNumber($('td.teamsNumber').text().split("+")[0])!
+
+    const allMatchesListed = !$('td.teamsNumber').text().includes("+")!
 
     const teams = $('.team-box')
       .toArray()
@@ -201,6 +204,7 @@ export const getEvent =
       prizePool,
       location,
       numberOfTeams,
+      allMatchesListed,
       teams,
       prizeDistribution,
       relatedEvents,
