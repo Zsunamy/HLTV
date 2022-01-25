b416d00b240cd92c1f87bea9a66c94dca64e803a
Nils Jacobs <niljaco.pi@gmail.com>
2022-01-25 21:45:54 +0100

    updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,8 @@ export const defaultLoadPage =
     new Promise<string>((resolve) => {
 				puppeteer.launch({
 				headless: true,
-				ignoreHTTPSErrors: true,
+				ignoreHTTPSErrors: true
+				args: ['--no-sandbox']
 			}).then((browser: any) => {
 				browser.newPage().then((page: any) => {
 					page.setUserAgent('Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36').then(() => {
