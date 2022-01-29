9e9dc2dfb56c8420cc1e4c20f72ad39d6a49749d
Nils Jacobs <niljaco.pi@gmail.com>
2022-01-30 00:16:38 +0100

    updated dependencies

--- src/config.ts
+++ src/config.ts
@@ -11,24 +11,21 @@ export interface HLTVConfig {
 export const defaultLoadPage =
   (httpAgent: HttpsAgent | HttpAgent | undefined) => (url: string) =>
     new Promise<string>((resolve) => {
-				puppeteer.launch({
-				headless: true,
-				ignoreHTTPSErrors: true,
-				args: ['--no-sandbox']
-			}).then((browser: any) => {
-				browser.newPage().then((page: any) => {
-					page.setUserAgent('Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36').then(() => {
-						page.goto(url).then(() => {
-							page.content().then((content: any) => {
-								browser.close().then(() => {
-									resolve(content)
-								})
-							})
-						})
-					})
-				})
-			})
-		})
+      request.get(
+        url,
+        {
+          gzip: true,
+          agent: httpAgent
+        },
+        (err, __, body) => {
+          if (err) {
+            throw err
+          }
+
+          resolve(body)
+        }
+      )
+    })
 
 const defaultAgent = new HttpsAgent()
 
